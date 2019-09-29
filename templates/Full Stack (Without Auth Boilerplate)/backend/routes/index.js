1e0f984e110322c1c384c109d22540c571bbf133
diurivj <diurivj@icloud.com>
2019-09-29 14:40:01 -0500

    fixed typo in backend folder for hostname

--- templates/Full Stack (Without Auth Boilerplate)/backend/routes/index.js
+++ templates/Full Stack (Without Auth Boilerplate)/backend/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({ msg: 'Working', hostname });
+  res.status(200).json({ msg: 'Working' });
 });
 
 module.exports = router;
